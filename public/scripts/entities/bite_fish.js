dd14b55f7d97e35c02ca5a413eeef9df8a5e4b23
Mitchell Pizzacalla <mtpizzacalla@gmail.com>
2021-03-12 19:59:25 -0500

    cleanup

--- public/scripts/entities/bite_fish.js
+++ public/scripts/entities/bite_fish.js
@@ -5,13 +5,10 @@ class BiteFish extends Fish {
   }
 
   onClick(event) {
-    console.log('Clicked BiteFish!');
     for (let proximateDenizen of this.tank.getProximateDenizens(this.position, 75)) { // Get array of nearby Denizens
       if (proximateDenizen instanceof SwitchFish || proximateDenizen instanceof GoFish) { // BiteFish only eat SwitchFish and GoFish.
         proximateDenizen.kill(); // :(
       }
     }
-    // console.log('getProximateDenizens:')
-    // console.log(res);
   }
 }
